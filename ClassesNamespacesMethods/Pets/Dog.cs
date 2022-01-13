4a2aef8f12d2259d39b8ce2ad7c55cb6e2fd006f
albertchitta <albert.w.chittaphong@gmail.com>
2022-01-12 21:47:34 -0600

    Add project files.

---
+++ ClassesNamespacesMethods/Pets/Dog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesNamespacesMethods.Pets
+{
+    //4 Pillars of Object Oriented Programming (OOP)
+    //Abstraction, Polymorphism, Inheritance, **Encapsulation** (APIE)
+
+    //internal is similar to private but it limits to only this project seeing it.
+    //other access modifiers are: public (allows access from anywhere), sealed (limited inheritance allowed), abstract (only another class can inherit from this), private (rarely used)
+    internal class Dog
+    {
+        //properties - TitleCase
+        //auto property - has a plain getter/setter
+        //Properties are private by default but can also be: public, readonly
+        //class is a custom type thus "Dog" is now a type.
+        //any custom property or type we add must be defined by us.
+
+        //access modifier   type    name    getter & setter
+        public string FurType { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public double Weight { get; set; }
+        public DateTime DateOfBirth { get; private set; }
+        //calculated property
+        public int Age
+        {
+            get
+            {
+                return (DateTime.Now - DateOfBirth).Days / 365;
+            }
+        }
+
+        //Fields - for private values needed by the class, but not needed outside the class
+        //does not require a git or set
+        //access modifier of private, a type, and a name
+        //name start with an "_" then it is camel case
+        private bool _isFertile;
+
+        //methods - TitleCase
+        //access modifier   return type     name    code
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} barks loudly.");
+        }
+
+        public void SetDateOfBirth(DateTime dob)
+        {
+            DateOfBirth = dob;
+        }
+
+        public int GiveBirth()
+        {
+            //if this does not use any properties or fields in the class, we can make this method "static" prior to the int
+            //static method is used across ALL instances and as such, you do not need to create an instance.
+            int numOfPuppies = new Random().Next();
+            if (_isFertile)
+            {
+                return numOfPuppies;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ ClassesNamespacesMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿using ClassesNamespacesMethods.Pets;
+using System;
+
+namespace ClassesNamespacesMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //namespaces
+            var dog1 = new Dog();
+
+            dog1.FurType = "short";
+            dog1.Name = "Fido";
+            dog1.Breed = "putbull";
+            dog1.Weight = 74.5;
+
+            Console.WriteLine($"{dog1.Name} is a {dog1.Breed} with {dog1.FurType} fur and weighs {dog1.Weight} pounds.");
+
+            Dog dog2 = new Dog();
+            dog2.FurType = "long";
+            dog2.Name = "Astro";
+            dog2.Breed = "huskie";
+            dog2.Weight = 1.2;
+
+            dog2.Bark();
+            dog1.Bark();
+            //dog1.DateOfBirth = DateTime.Now.AddDays(-1000);
+            DateTime today = DateTime.Now;
+            DateTime fiveDaysAgo = DateTime.Now.AddDays(-5);
+
+            dog1.SetDateOfBirth(DateTime.Now.AddDays(-1000));
+            Console.WriteLine($"{dog1.Name} is {dog1.Age} years old.");
+        }
+    }
+}
